b0a2219fa1159eb32fd1dba8ad1ac5f91b6ee03e
kurniawan.umam <kurniawan.umam@pegadaian.co.id>
2021-04-03 19:46:17 +0700

    [STAGING-HMVC][AAT] : replace db

--- db_aat.sql
+++ db_aat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 16, 2021 at 07:34 PM
+-- Generation Time: Apr 03, 2021 at 02:45 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.2.23
 
@@ -62,6 +62,37 @@ CREATE TABLE `tbl_menu` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_no_surat`
+--
+
+CREATE TABLE `tbl_no_surat` (
+  `id_surat` int(11) NOT NULL,
+  `nomor_inc` int(11) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `key_perusahaan` varchar(10) NOT NULL,
+  `bulan` varchar(10) NOT NULL,
+  `tahun` int(11) NOT NULL,
+  `combine` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_no_surat`
+--
+
+INSERT INTO `tbl_no_surat` (`id_surat`, `nomor_inc`, `type`, `key_perusahaan`, `bulan`, `tahun`, `combine`) VALUES
+(2, 1, 'SJ', 'SSM', 'IV', 21, '001/SSM/SJ/IV/21'),
+(3, 2, 'SJ', 'SSM', 'IV', 21, '002/SSM/SJ/IV/21'),
+(4, 3, 'SJ', 'SSM', 'IV', 21, '003/SSM/SJ/IV/21'),
+(5, 4, 'SJ', 'SSM', 'IV', 21, '004/SSM/SJ/IV/21'),
+(6, 5, 'SJ', 'SSM', 'IV', 21, '005/SSM/SJ/IV/21'),
+(7, 1, 'SJ', 'AAT', 'IV', 21, '001/AAT/SJ/IV/21'),
+(8, 2, 'SJ', 'AAT', 'IV', 21, '002/AAT/SJ/IV/21'),
+(9, 6, 'SJ', 'SSM', 'IV', 21, '006/SSM/SJ/IV/21'),
+(10, 1, 'BJ', 'SSM', 'IV', 21, '001/SSM/BJ/IV/21');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_previllage`
 --
 
@@ -82,8 +113,49 @@ CREATE TABLE `tbl_proyek` (
   `nama_proyek` varchar(100) NOT NULL,
   `alamat_proyek` text NOT NULL,
   `nama_pic` varchar(50) NOT NULL,
-  `no_hp_pic` varchar(15) NOT NULL
+  `no_hp_pic` varchar(15) NOT NULL,
+  `id_pt` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pt`
+--
+
+CREATE TABLE `tbl_pt` (
+  `id_pt` int(11) NOT NULL,
+  `nama_pt` varchar(50) NOT NULL,
+  `key_pt` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_pt`
+--
+
+INSERT INTO `tbl_pt` (`id_pt`, `nama_pt`, `key_pt`) VALUES
+(1, 'CV. ANUGRAH AGUNG TEKNIK', 'AAT'),
+(2, 'PT. SIDDHAKARYYA SATUHU MUKTI', 'SSM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_satuan`
+--
+
+CREATE TABLE `tbl_satuan` (
+  `id_satuan` int(11) NOT NULL,
+  `nama_satuan` varchar(20) NOT NULL,
+  `key_satuan` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_satuan`
+--
+
+INSERT INTO `tbl_satuan` (`id_satuan`, `nama_satuan`, `key_satuan`) VALUES
+(1, 'Kg', 'Kilogram'),
+(2, 'Buah', 'buah');
 
 -- --------------------------------------------------------
 
@@ -100,6 +172,28 @@ CREATE TABLE `tbl_user` (
   `alamat` text NOT NULL,
   `role` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama`, `email`, `no_hp`, `password`, `alamat`, `role`) VALUES
+(1, 'Umam', 'admin', '0899225545', '21232f297a57a5a743894a0e4a801fc3', 'admin', '1'),
+(2, 'fadfadnk', 'umam.tekno@gmail.com', '081290766692', 'fdafaf', 'Jln. R.A Kartini No. 36 Yukum jaya Terbanggi Besar Lamteng', '1'),
+(3, 'fadfa', 'fdafad@gmail.com', '4343434', '43434', '34', '1'),
+(4, 'fadfa', 'fdafad@gmail.com', '4343434', '43434', '34', '1'),
+(5, 'fdafa', 'daf@gmail.com', '081290766692', 'fadfa', 'Jln. R.A Kartini No. 36 Yukum jaya Terbanggi Besar Lamteng', '1'),
+(6, 'fdfd', 'umam.tekno@gmail.com', '081290766692', 'fdfd', 'Jln. R.A Kartini No. 36 Yukum jaya Terbanggi Besar Lamteng', '1'),
+(7, 'fdfd', 'umam.tekno@gmail.com', '081290766692', 'afdaf', 'Jln. R.A Kartini No. 36 Yukum jaya Terbanggi Besar Lamteng', '1'),
+(8, 'fdfd', 'umam.tekno@gmail.com', '081290766692', 'afdaf', 'Jln. R.A Kartini No. 36 Yukum jaya Terbanggi Besar Lamteng', '1'),
+(9, 'dafd', 'umam.tekno@gmail.com', '081290766692', 'fadfa', 'Jln. R.A Kartini No. 36 Yukum jaya Terbanggi Besar Lamteng', '1'),
+(10, 'casas', 'umam.tekno@gmail.com', '081290766692', 'dsadas', 'Jln. R.A Kartini No. 36 Yukum jaya Terbanggi Besar Lamteng', '1'),
+(11, 'dfafa', 'fasf@mal.com', '431413', 'fafaf', 'fadfad', '1'),
+(12, 'dsafad', 'adfad@m.com', '53242', 'fdfs', 'dsfsd', '1'),
+(13, 'fadfadf', 'umam.tekno@gmail.com', '081290766692', 'fdaf', 'Jln. R.A Kartini No. 36 Yukum jaya Terbanggi Besar Lamteng', '1'),
+(14, 'fafadf', 'umam.tekno@gmail.com', '081290766692', 'fdfd', 'Jln. R.A Kartini No. 36 Yukum jaya Terbanggi Besar Lamteng', '1'),
+(15, 'fafadf', 'umam.tekno@gmail.com', '081290766692', 'fdfd', 'Jln. R.A Kartini No. 36 Yukum jaya Terbanggi Besar Lamteng', '1'),
+(16, 'fadfadnk', 'umam.tekno@gmail.com', '081290766692', 'fdafd', 'Jln. R.A Kartini No. 36 Yukum jaya Terbanggi Besar Lamteng', '1');
 
 --
 -- Indexes for dumped tables
@@ -124,6 +218,12 @@ ALTER TABLE `tbl_menu`
   ADD PRIMARY KEY (`id_menu`);
 
 --
+-- Indexes for table `tbl_no_surat`
+--
+ALTER TABLE `tbl_no_surat`
+  ADD PRIMARY KEY (`id_surat`);
+
+--
 -- Indexes for table `tbl_previllage`
 --
 ALTER TABLE `tbl_previllage`
@@ -134,6 +234,18 @@ ALTER TABLE `tbl_previllage`
 --
 ALTER TABLE `tbl_proyek`
   ADD PRIMARY KEY (`id_proyek`);
+
+--
+-- Indexes for table `tbl_pt`
+--
+ALTER TABLE `tbl_pt`
+  ADD PRIMARY KEY (`id_pt`);
+
+--
+-- Indexes for table `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  ADD PRIMARY KEY (`id_satuan`);
 
 --
 -- Indexes for table `tbl_user`
@@ -164,6 +276,12 @@ ALTER TABLE `tbl_menu`
   MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tbl_no_surat`
+--
+ALTER TABLE `tbl_no_surat`
+  MODIFY `id_surat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT for table `tbl_previllage`
 --
 ALTER TABLE `tbl_previllage`
@@ -176,10 +294,22 @@ ALTER TABLE `tbl_proyek`
   MODIFY `id_proyek` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tbl_pt`
+--
+ALTER TABLE `tbl_pt`
+  MODIFY `id_pt` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  MODIFY `id_satuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
